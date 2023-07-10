e0e81b1603cc3ae946c4a501acdfe6888b610b67
Andrew Deason <deasonam@gmail.com>
2023-07-10 11:04:05 -0500

    lc coding challenge #2

---
+++ week2.js
@@ -0,0 +1,10 @@
+let test = "I love to code"
+
+function FirstReverse(str) { 
+
+  // code goes here
+  return str.split('').reverse().join('');
+}
+   
+// keep this function call here 
+console.log(FirstReverse(test));
